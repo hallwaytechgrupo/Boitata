e08bb11806d63b18aeb55388827130f810a773f5
Christopher <contato.christopher@gmail.com>
2025-04-16 18:14:39 -0300

    chore: add package json and map loading on brazil

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,32 +1,57 @@
-import { useRef, useEffect } from 'react'
-import mapboxgl from 'mapbox-gl'
+import { useRef, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-import './App.css'
+import './App.css';
 
 function App() {
-
-  const mapRef = useRef();
-  const mapContainerRef = useRef();
+  const mapRef = useRef<mapboxgl.Map | null>(null);
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYm9pdGF0YSIsImEiOiJjbTlrZGF3ejgwb2FxMnJvYWZ1Z3pudndpIn0.EiV7WmRDDZZBkY2A0PSJ1A'
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYm9pdGF0YSIsImEiOiJjbTlrZGF3ejgwb2FxMnJvYWZ1Z3pudndpIn0.EiV7WmRDDZZBkY2A0PSJ1A';
     mapRef.current = new mapboxgl.Map({
-      container: mapContainerRef.current,
+      container: mapContainerRef.current!,
       style: 'mapbox://styles/mapbox/dark-v11',
+      center: [-55.491477, -13.720512],
+      zoom: 4,
+      attributionControl: false,
+      logoPosition: 'top-right',
+    });
+
+    mapRef.current.on('load', () => {
+     
+      mapRef.current.addSource('country-boundaries', {
+        type: 'vector',
+        url: 'mapbox://mapbox.country-boundaries-v1',
+      });
+
+     
+      mapRef.current.addLayer({
+        id: 'brazil-boundary',
+        type: 'fill',
+        source: 'country-boundaries',
+        'source-layer': 'country_boundaries',
+        filter: ['==', 'iso_3166_1', 'BR'],
+        paint: {
+          'fill-color': '#088',
+          'fill-opacity': 0.1,
+          'fill-outline-color': '#000',
+        },
+      });
     });
 
     return () => {
-      mapRef.current.remove()
-    }
+      mapRef.current?.remove();
+    };
   }, []);
 
   return (
     <>
-      <div id='map-container' ref={mapContainerRef}/>
+      <div id="map-container" ref={mapContainerRef} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
